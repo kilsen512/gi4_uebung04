93aab7689263a03075eaccc3fdc37757a86ebb37
kilsen512 <kilian.pfeiffer@rwth-aachen.de>
2015-05-19 20:53:25 +0200

    added c++ version

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <iostream>
+
+int fibonacci(int n) {
+	if(n == 1)
+		return 1;
+	else if(n == 0)
+		return 0;
+	else
+		return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+	int x = fibonacci(10);
+	std::cout << x ;
+}
+
+
